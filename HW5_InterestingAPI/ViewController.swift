20d38b7f784661564ef64d150e07513087929b4b
gurseskerem <keremgurses1997@gmail.com>
2021-03-19 15:56:28 +0300

    Changes

--- HW5_InterestingAPI/ViewController.swift
+++ HW5_InterestingAPI/ViewController.swift
@@ -10,15 +10,66 @@ import ARKit
 
 class ViewController: UIViewController {
 
+    
     @IBOutlet weak var counterLabel: UILabel!
     @IBOutlet weak var sceneView: ARSCNView!
+    
+    var counter:Int = 0 {
+        didSet {
+            counterLabel.text = "\(counter)"
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         let scene = SCNScene()
         sceneView.scene = scene
-        // Do any additional setup after loading the view.
+        
     }
+    
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        let configuration = ARWorldTrackingConfiguration()
+        sceneView.session.run(configuration)
+        addObject()
+        
+    }
+    func addObject() {
+        let imposter = ARModel()
+        imposter.loadModal()
+        imposter.scale = SCNVector3(0.09,0.09,0.09)
+        let xPos = randomPosition(lowerBound: -1.5, upperBound: 1.5)
+        let yPos = randomPosition(lowerBound: -1.5, upperBound: 1.5)
+        imposter.position = SCNVector3(xPos, yPos, -5)
+        sceneView.scene.rootNode.addChildNode(imposter)
+    }
+    
+    func randomPosition (lowerBound lower:Float, upperBound upper:Float) -> Float {
+        return Float(arc4random()) / Float(UInt32.max) * (lower - upper) + upper
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if let touch = touches.first{
+            let location = touch.location(in: sceneView)
+            let hitList = sceneView.hitTest(location, options: nil)
+            
+            if let hitObject = hitList.first{
+                let node=hitObject.node
+                if node.name == "ARModel" {
+                    counter += 1
+                    node.removeFromParentNode()
+                    addObject()
+                }
+            }
+        }
 
+    }
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
 
 }
 
